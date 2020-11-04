0720731e9813840353a0902b9a483b4265ae932b
xlimiii <dominika.limanowka10@gmail.com>
2020-11-05 00:50:26 +0100

    welcome message at a server

--- SkradzioneWzoryWeb/models.py
+++ SkradzioneWzoryWeb/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField() #w przyszłości title i text do zmiany na plik
+    created_date = models.DateTimeField(
+        default=timezone.now)
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.title
