4080a87c7a0d9557ef0e90f3f1d0ce233add9044
xlimiii <dominika.limanowka10@gmail.com>
2020-11-11 19:43:58 +0100

    Dodanie wyboru pliku z dysku

--- SkradzioneWzoryWeb/views.py
+++ SkradzioneWzoryWeb/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def welcome_start(request):
+    if request.method == 'POST':
+        uploaded_file = request.FILES['document']
+        print(uploaded_file.name)
+        print(uploaded_file.size)
     return render(request, 'SkradzioneWzoryWeb/welcome.html', {})
