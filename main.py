dfa4c673c7e84a9b6bcc695c300f1d090977cfd1
Wojciech Kozieł <44378819+fern00@users.noreply.github.com>
2020-11-26 17:23:16 +0100

    code cleaning and list fix

--- main.py
+++ main.py
@@ -1,58 +1,79 @@
 import os, re
 
 class Files:
+
     def __init__(self, data, name):
+        """Tworzy obiekt pliku zawierający nazwę i listę wzorów"""
         self.name = name
         self.math = []
         self._prepare_data(data)
 
     def _prepare_data(self, data):
+        """Usuwa wszystkie znaki białe i mnienia tekst w jeden ciąg"""
         data = " ".join(data.split())
         data = data.replace(" ", "")
         self._separate_formulas(data)
 
     def _separate_formulas(self, data):
-        print("Dane dla pliku: "+self.name)
-        # self.math.append(re.findall(r"\$(.+?)\$", data)) #$...$
-        self.math.append(re.findall(r"\$\$(.+?)\$\$", data))
-        self.math.append(re.findall(r"\\\[(.+?)\\\]", data))
-        self.math.append(re.findall(r"\\\((.+?)\\\)", data))
-        self.math.append(re.findall(r"\\begin\{displaymath\}(.+?)\\end\{displaymath\}", data))
-        self.math.append(re.findall(r"\\begin\{math\}(.+?)\\end\{math\}", data))
-        self.math.append(re.findall(r"\\begin\{equation\}(.+?)\\end\{equation\}", data))
-        self.math.append(re.findall(r"\\begin\{equation\*\}(.+?)\\end\{equation\*\}", data))
-        self.math.append(re.findall(r"\\begin\{eqnarray\}(.+?)\\end\{eqnarray\}", data))
-        self.math.append(re.findall(r"\\begin\{eqnarray\*\}(.+?)\\end\{eqnarray\*\}", data))
-        self.math.append(re.findall(r"\\begin\{align\}(.+?)\\end\{align\}", data))
-        self.math.append(re.findall(r"\\begin\{align\*\}(.+?)\\end\{align\*\}", data))
-        self.math.append(re.findall(r"\\begin\{multline\}(.+?)\\end\{multline\}", data))
-        self.math.append(re.findall(r"\\begin\{multline\*\}(.+?)\\end\{multline\*\}", data))
-        self.math.append(re.findall(r"\\begin\{gather\}(.+?)\\end\{gather\}", data))
-        self.math.append(re.findall(r"\\begin\{gather\*\}(.+?)\\end\{gather\*\}", data))
-        self.math = [x for x in self.math if x != []] #usuwa puste
-        print(self.math)
-        print(" ")
+        """Wyszukuje wzory pomiędzy zadanymi komendami i zapisuje w liście jednowymiarowej math"""
+        list = re.findall(r"\$\$(.+?)\$\$", data)
+        self.math.extend(list)
+        list = re.findall(r"\\\[(.+?)\\\]", data)
+        self.math.extend(list)
+        list = re.findall(r"\\\((.+?)\\\)", data)
+        self.math.extend(list)
+        list = re.findall(r"\\begin\{displaymath\}(.+?)\\end\{displaymath\}", data)
+        self.math.extend(list)
+        list = re.findall(r"\\begin\{math\}(.+?)\\end\{math\}", data)
+        self.math.extend(list)
+        list = re.findall(r"\\begin\{equation\}(.+?)\\end\{equation\}", data)
+        self.math.extend(list)
+        list = re.findall(r"\\begin\{equation\*\}(.+?)\\end\{equation\*\}", data)
+        self.math.extend(list)
+        list = re.findall(r"\\begin\{eqnarray\}(.+?)\\end\{eqnarray\}", data)
+        self.math.extend(list)
+        list = re.findall(r"\\begin\{eqnarray\*\}(.+?)\\end\{eqnarray\*\}", data)
+        self.math.extend(list)
+        list = re.findall(r"\\begin\{align\}(.+?)\\end\{align\}", data)
+        self.math.extend(list)
+        list = re.findall(r"\\begin\{align\*\}(.+?)\\end\{align\*\}", data)
+        self.math.extend(list)
+        list = re.findall(r"\\begin\{multline\}(.+?)\\end\{multline\}", data)
+        self.math.extend(list)
+        list = re.findall(r"\\begin\{multline\*\}(.+?)\\end\{multline\*\}", data)
+        self.math.extend(list)
+        list = re.findall(r"\\begin\{gather\}(.+?)\\end\{gather\}", data)
+        self.math.extend(list)
+        list = re.findall(r"\\begin\{gather\*\}(.+?)\\end\{gather\*\}", data)
+        self.math.extend(list)
+        self._print_stats(data)
+
+    def _print_stats(self, data):
+        """Szybkie dane dla szukania błędów i testowania"""
+        print("DANE Z PLIKU: "+self.name+"\n"+data) #Print nazyw i tekstu z plików
+        print("\nWZORY:"+str(self.math)) #Print listy z wzorami
+
 
 class File_Reader:
 
     def __init__(self):
+        """Wywołuje wczytywanie plików z /database i przechowuje tablice sprawdzonych plików"""
         self.files = []
-        self.files_names = []
-        self._names_download()
         self._read_file()
 
     def _names_download(self):
-        self.files_names = os.listdir('database')
+        """Zwraca nazwy plików znajdujących się w /database"""
+        return os.listdir('database')
 
 
     def _read_file(self):
-        print("Wczytywanie plików: "+str(self.files_names)+"\n")
-        for file in self.files_names:
+        """Zwraca nazwy plików znajdujących się w /database"""
+        file_names = self._names_download()
+        for file in file_names:
             f = open("database/" + file,"r")
-            self.files.append(Files(f.read(), str(file)))
+            self.files.append(Files(f.read(), file))
 
 
 def main():
     reader = File_Reader()
-
 main()
