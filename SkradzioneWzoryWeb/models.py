7dbfea61dd2261d429855a73bfdbd5c97425eebb
xlimiii <dominika.limanowka10@gmail.com>
2020-12-10 10:00:48 +0100

    Pierwsze próby dołączania logiki biznesowej do projektu

--- SkradzioneWzoryWeb/models.py
+++ SkradzioneWzoryWeb/models.py
@@ -1,6 +1,8 @@
+from typing import re
+from django.core.files.storage import FileSystemStorage
 from django.db import models
 from django.utils import timezone
-
+from .models import *
 class File(models.Model):
     name = models.CharField(max_length=200, null=True)
     author = models.CharField(max_length=200, null=True)
@@ -9,3 +11,55 @@ class File(models.Model):
 
     def __str__(self):
         return self.name
+    def list_all_files_from_database(self):
+        files=[]
+        all_files = File.objects.all()
+        for file in all_files:
+            files.append(file)
+        return files
+
+    def _prepare_data(self, data):
+        """Usuwa wszystkie znaki białe i zmienia tekst w jeden ciąg"""
+        data = " ".join(data.split())
+        data = data.replace(" ", "")
+        self._separate_formulas(data)
+
+    def _separate_formulas(self, data):
+        """Wyszukuje wzory pomiędzy zadanymi komendami i zapisuje w liście jednowymiarowej math"""
+        list = re.findall(r"\$\$(.+?)\$\$", data)
+        self.math.extend(list)
+        list = re.findall(r"\\\[(.+?)\\\]", data)
+        self.math.extend(list)
+        list = re.findall(r"\\\((.+?)\\\)", data)
+        self.math.extend(list)
+        list = re.findall(r"\\begin\{displaymath\}(.+?)\\end\{displaymath\}", data)
+        self.math.extend(list)
+        list = re.findall(r"\\begin\{math\}(.+?)\\end\{math\}", data)
+        self.math.extend(list)
+        list = re.findall(r"\\begin\{equation\}(.+?)\\end\{equation\}", data)
+        self.math.extend(list)
+        list = re.findall(r"\\begin\{equation\*\}(.+?)\\end\{equation\*\}", data)
+        self.math.extend(list)
+        list = re.findall(r"\\begin\{eqnarray\}(.+?)\\end\{eqnarray\}", data)
+        self.math.extend(list)
+        list = re.findall(r"\\begin\{eqnarray\*\}(.+?)\\end\{eqnarray\*\}", data)
+        self.math.extend(list)
+        list = re.findall(r"\\begin\{align\}(.+?)\\end\{align\}", data)
+        self.math.extend(list)
+        list = re.findall(r"\\begin\{align\*\}(.+?)\\end\{align\*\}", data)
+        self.math.extend(list)
+        list = re.findall(r"\\begin\{multline\}(.+?)\\end\{multline\}", data)
+        self.math.extend(list)
+        list = re.findall(r"\\begin\{multline\*\}(.+?)\\end\{multline\*\}", data)
+        self.math.extend(list)
+        list = re.findall(r"\\begin\{gather\}(.+?)\\end\{gather\}", data)
+        self.math.extend(list)
+        list = re.findall(r"\\begin\{gather\*\}(.+?)\\end\{gather\*\}", data)
+        self.math.extend(list)
+        list = re.findall(r"\$([^$]*)\$", data)
+        list = [elem for elem in list if elem != ""]
+        self.math.extend(list)
+
+        self._print_stats(data)
+
+
--- SkradzioneWzoryWeb/views.py
+++ SkradzioneWzoryWeb/views.py
@@ -1,11 +1,20 @@
+import os
+from io import BytesIO
+
 from django.http import HttpResponse
 from django.shortcuts import render
+from SkradzioneWzoryWeb.models import File
 
 def run_algorithms (request):
+
     if request.method == 'POST':
         uploaded_file = request.FILES['document']
-        print(uploaded_file.name)
-        print(uploaded_file.size)
+        user_file = File()
+        context = user_file.list_all_files_from_database()
+        print("Pliki dostepne w bazie danych to: ", context)
+        #drukuję w konsoli zawartość pobranego przez użytkownika pliku
+        print(uploaded_file.read().decode())
+        return render(request, 'SkradzioneWzoryWeb/run.html', {'instance': user_file})
     return render(request, 'SkradzioneWzoryWeb/run.html', {})
 
 def about_project(request):
