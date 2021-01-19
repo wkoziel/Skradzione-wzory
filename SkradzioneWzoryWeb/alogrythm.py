de3fd150257add2a4d7609ef57667cb21a17416c
Wojciech Kozieł <44378819+fern00@users.noreply.github.com>
2021-01-20 00:22:31 +0100

    final

--- SkradzioneWzoryWeb/alogrythm.py
+++ SkradzioneWzoryWeb/alogrythm.py
@@ -4,9 +4,11 @@ from PIL import Image
 from sympy import preview
 import imagehash
 import requests
+import time
 
 def cut_out_math(data):
-    """Wycina wzory z pliku"""
+    """Oczyszcza plik z problematycznych znaków i zwraca listę wzorów zawartych w pliku"""
+
     data = " ".join(data.split())
     math = []
     list = findall(r"\$\$(.+?)\$\$", data)
@@ -47,10 +49,11 @@ def cut_out_math(data):
     return math
 
 def create_list_of_hashes(math):
-    """Tworzy hashe z wczytywanego pliku"""
+    """Dla zadanej listy wzorów zwraca listę hashy wygenerowanych na podstwie graficznej reprezentacji wzoru"""
+
     math_hash = []
     for i in math:
-        print("Przetwarzane: "+i)
+        print("Przetwarzanie wzory:"+i)
         byteImgIO = BytesIO()
         preview(r'$$' + i + '$$', output='png', viewer='BytesIO', outputbuffer=byteImgIO, packages=("polski", "inputenc"), euler=False)
         img = Image.open(byteImgIO)
@@ -59,26 +62,27 @@ def create_list_of_hashes(math):
     return math_hash
 
 def get_file_from_database(path):
-    """Zamienia zdjęcie z bazy na hash"""
+    """Pobiera graficzną reprezentacje wzoru i zwraca jego hash"""
+
     #base = "http://skradzionewzory.pythonanywhere.com/static/database/"
     base = "http://127.0.0.1:8000/static/database/"
-    url = base + path
-    print("Otwieram:"+url)
-    response = requests.get(url)
+    response = requests.get(base + path)
     img = Image.open(BytesIO(response.content))
     hash = imagehash.average_hash(img)
     return hash
 
 def get_hashes_for_file(dir):
     """Wywołuje pobranie hashu dla każdego pliku z folderu"""
+
     hash_db = []
-    for i in range(1,10):
+    for i in range(1,11):
         file = str(i)+".png"
         hash_db.append([dir+"/"+file, get_file_from_database(dir+"/"+file)])
     return hash_db
 
 def compare_hashes(loaded_file_data):
     """Metoda porównująca zawartość bazy i plik źródłowy"""
+
     #Zestaw folderów z bazy danych
     files = []
     for i in range(1,51):
@@ -89,33 +93,55 @@ def compare_hashes(loaded_file_data):
 
     results = []
     for file in files:
+        #Pętla przeszukuje baze danych ex1, ex2, ex3
         math_to_compare = get_hashes_for_file(file)
         match_data = []
         matches = 0
-        for i in loaded_file_data:
+        for wzor1 in loaded_file_data:
+            #Pętla przeszukuje wzory zadane w pliku
+            tmp_diff_num = []
+            tmp_match_data = []
+            for wzor2 in math_to_compare:
+                #Pętla przeszukuje wzory zadane w bazie dla aktualnego exX
+                diff = wzor1[1] - wzor2[1]
+                if diff < 9:
+                    sim = int(((64 - diff) / 64) * 100)
+                    tmp_diff_num.append(diff)
+                    tmp_match_data.append([wzor1[0], base+wzor2[0], sim])
+            min = -1
+            if tmp_match_data:
+                matches += 1
+                index = 0
+                min = tmp_diff_num[0]
+                for i in range(0, len(tmp_diff_num)):
+                    if min > tmp_diff_num[i]:
+                        index = i
+                        min = tmp_diff_num[i]
 
-            for j in math_to_compare:
-                diff = i[1] - j[1]
-                if diff < 8:
-                    matches += 1
-                    sim = 100 - 2 * diff
-                    match_data.append([i[0], base+j[0], sim])
-                    break
-        is_enaugh = len(loaded_file_data) / 2
+                match_data.append(tmp_match_data[index])
+
+        is_enaugh = len(loaded_file_data) // 2
         if match_data and matches > is_enaugh:
             similarity = int(matches / len(loaded_file_data) * 100)
             results.append([file, match_data, similarity])
+
     return results
 
 
 def get_file_math(data):
+    """Zwraca listę wzorów z zadanego pliku"""
+
     return cut_out_math(data)
 
 def get_file_hash(data):
+    """Zwraca liste hashy z zadanego pliku"""
+
     math = cut_out_math(data)
     return create_list_of_hashes(math)
 
 def get_loaded_file_data(data):
+    """Przetwarza zadany plik by uzyskać listę wzoró oraz ich hashy"""
+
     loaded_file_data = []
     math = cut_out_math(data)
     math_hash = create_list_of_hashes(math)
@@ -124,6 +150,11 @@ def get_loaded_file_data(data):
     return loaded_file_data
 
 def alghoritm(data):
+    """Główny algorytm kontolujący etapy postępowania programu"""
+
+    start = time.time()
     loaded_file_data = get_loaded_file_data(data)
     results = compare_hashes(loaded_file_data)
+    end = time.time()
+    print("Wykonanie algorytmu zajęło: "+str(end - start)+" sekund.")
     return results
